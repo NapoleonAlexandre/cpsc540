f6052f61258ca813834fbf5f6a0e3c3ca4bcd3f1
Thibaut Astic <thast@eos.ubc.ca>
2017-03-01 14:54:27 -0800

    add comments

--- assn3/a3/MixtureBernoulli.m
+++ assn3/a3/MixtureBernoulli.m
@@ -1,37 +1,38 @@
 function [ model ] = MixtureBernoulli(X,alpha,beta,k,its)
 
 [n,d] = size(X);
-theta = mean(X);
 model.predict = @predict;
 model.sample = @sample;
 
+% Initialization of the parameters \pi_c and \mu_ij
 pic = ones(k,1)/k;
 mu = 0.25+0.5*rand(k,d);
 
+%Iterations
 for j=1:its
     
-    % E
+    % E-step
     L = zeros(n,k);
     r = zeros(n,k);
     for i=1:k
         L(:,i) = sum(log(pic(i)*(mu(i,:).^X).*((1-mu(i,:)).^(1-X))),2);
     end
     
-    Lmax = max(max(L));
     %LogSumExp trick
+    Lmax = max(max(L));
     r = L-Lmax-log(sum(exp(L-Lmax),2));
     r = exp(r);
     
     %M step
     z = sum(r);
+    %Update \mu
     for i=1:k
         mu(i,:) = (sum(spdiags(r(:,i),0,n,n)*X)+beta-1)/(z(i)+k*(beta-1));
     end
+    %update \pi_c
     pic = (z+alpha-1)/(n+k*(alpha-1));
 
 end
-model.predict = @predict;
-model.sample = @sample;
 model.mu = mu;
 model.pic = pic;
 model.r = r;
@@ -45,6 +46,7 @@ function samples = sample(model,t)
 
 samples = zeros(t,d);
 for i = 1:t
+    %Pick a random cluster
     k =sampleDiscrete(model.pic);
     samples(i,:) = rand(1,d) < model.mu(k,:);
     end
